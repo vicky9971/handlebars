7feb1aeacd982b4568bfd0e3d9058dd54ac63eed
Vikas Chaudhary <vikas.chaudhary@tothenew.com>
2017-06-20 22:34:36 +0530

    Exercise Added

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//Global variable to hold all the pages authored.
+var pages=[];
+
+$(document).ready(function(){
+
+	$('.add-blog').on('click',function(){
+        // XXX write code here to compile handlebars template, generate json data and use it to render the same in HTML 
+	});
+});
